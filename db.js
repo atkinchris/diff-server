e223b0dc27d0df2b5658d3e39c2fd91d71618f63
Chris Atkin <atkinchris@gmail.com>
2018-02-28 16:12:34 +0000

    Add basic DB functions

--- db.js
+++ db.js
@@ -1,22 +1,91 @@
 const knex = require('knex')
+const uuid = require('uuid')
 
-const buildDb = () => {
+const now = () => (new Date()).toISOString()
+
+const buildDb = async () => {
   const db = knex({
     client: 'sqlite3',
     connection: { filename: './db.sqlite' },
     useNullAsDefault: true,
   })
 
-  db.schema.createTable('users', (table) => {
-    table.increments('id').primary()
-    table.string('nomination').notNullable()
-    table.string('space').notNullable()
-    table.integer('shelf').notNullable()
-    table.string('sku')
-    table.string('comment')
-    table.dateTime('effective_from').notNullable()
-    table.dateTime('effective_to').nullable()
-  })
+  const tableName = 'nominations'
+
+  const tableExists = await db.schema.hasTable(tableName)
+
+  if (!tableExists) {
+    await db.schema.createTable(tableName, (table) => {
+      table.increments('id').primary()
+      table.string('nomination').notNullable()
+      table.string('space').notNullable()
+      table.integer('shelf').notNullable()
+      table.string('sku')
+      table.string('comment')
+      table.dateTime('effective_from').notNullable()
+      table.dateTime('effective_to')
+    })
+  }
+
+  const all = () => db.select().from(tableName)
+
+  const select = id => db
+    .select()
+    .from(tableName)
+    .where({ nomination: id })
+    .orderBy('effective_from', 'desc')
+
+  const insert = (body) => {
+    const nomination = Object.assign({}, body, {
+      effective_from: now(),
+      nomination: uuid.v4(),
+    })
+    return db.insert(nomination).into(tableName)
+  }
+
+  const remove = async (id) => {
+    const results = await select(id)
+    const latest = results[0]
+
+    return db
+      .from(tableName)
+      .where({ id: latest.id })
+      .update({ effective_to: now() })
+  }
+
+  const update = async (id, body) => {
+    const results = await select(id)
+    const latest = results[0]
+    const next = Object.assign({}, latest, body, {
+      id: undefined,
+      nomination: id,
+      effective_from: now(),
+      effective_to: null,
+    })
+
+    return db.transaction(async (trx) => {
+      if (!latest.effective_to) {
+        await trx
+          .from(tableName)
+          .where({ nomination: id })
+          .update({ effective_to: next.effective_from })
+      }
+
+      await trx
+        .insert(next)
+        .into(tableName)
+
+      return trx
+    })
+  }
+
+  return {
+    all,
+    select,
+    insert,
+    remove,
+    update,
+  }
 }
 
 module.exports = buildDb
--- index.js
+++ index.js
@@ -2,24 +2,68 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const buildDb = require('./db')
 
-const app = express()
-const port = process.env.PORT || 8080
-const db = buildDb()
+const server = async () => {
+  const app = express()
+  const port = process.env.PORT || 8080
+  const db = await buildDb()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-  res.json('Hello World!')
-})
+  app.get('/', async (req, res) => {
+    const nominations = await db.all()
 
-app.post('/', (req, res) => {
-  const { body } = req
+    res.json(nominations)
+  })
 
-  res.json(body)
-})
+  app.get('/:id', async (req, res) => {
+    const { id } = req.params
+    const entries = await db.get(id)
 
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server start on port ${port}`)
-})
+    res.json(entries)
+  })
+
+  app.post('/', async (req, res) => {
+    const { body } = req
+
+    try {
+      await db.insert(body)
+      res.sendStatus(200)
+    } catch (error) {
+      console.log(error)
+      res.status(400).json(error)
+    }
+  })
+
+  app.post('/:id', async (req, res) => {
+    const { body } = req
+    const { id } = req.params
+
+    try {
+      await db.update(id, body)
+      res.sendStatus(200)
+    } catch (error) {
+      console.log(error)
+      res.status(400).json(error)
+    }
+  })
+
+  app.delete('/:id', async (req, res) => {
+    const { id } = req.params
+
+    try {
+      await db.remove(id)
+      res.sendStatus(200)
+    } catch (error) {
+      console.log(error)
+      res.status(400).json(error)
+    }
+  })
+
+  app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server start on port ${port}`)
+  })
+}
+
+server()
