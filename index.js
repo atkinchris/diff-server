12f7c12bd802b9c0864980766da86c53d9efe105
Chris Atkin <atkinchris@gmail.com>
2018-02-28 14:48:44 +0000

    Add basic Express server

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.json('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  const { body } = req
+
+  res.json(body)
+})
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server start on port ${port}`)
+})
